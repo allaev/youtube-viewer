ce97a9b36d7e366ee0d06f3af542eca7c6a02976
allaev <34710484+allaev@users.noreply.github.com>
2018-07-08 23:17:58 +0500

    create the event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,8 @@
 import React, {Component} from 'react';
 
-// const SearchBar = () => {
-//   return <input/>
-// };
-
 class SearchBar extends Component {
   render() {
-    return <input/>;
+    return <input onChange={event=>console.log(event.target.value)}/>;
   }
 }
 
