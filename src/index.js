875ca6a0e20c7295532c32585f741c04cb556812
allaev <34710484+allaev@users.noreply.github.com>
2018-07-09 16:26:19 +0500

    refactor to a class component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -6,17 +6,25 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = ''; //Put your API key here
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
-
 //Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state={ videos:[] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos)=>{
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
